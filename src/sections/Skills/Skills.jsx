65440871a6b2731bda2fef6addf59c4a33512671
markcy/encyklopdia <marksy.petras7@gmail.com>
2024-06-05 11:38:12 +0800

    done skills styling

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -5,7 +5,7 @@ import SkillList from "../../common/SkillList";
 function Skills() {
   return (
     <section id="skills" className={styles.container}>
-      <h1>Skills</h1>
+      <h1 className='sectionTitle'>Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill='HTML' />
         <SkillList src={checkMarkIcon} skill='CSS' />

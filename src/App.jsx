83dee01c3d451174ac6368769b2a0b54a67404fa
markcy/encyklopdia <marksy.petras7@gmail.com>
2024-06-06 19:04:07 +0800

    addition of contact components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Projects />
       <Skills />
+      <Contact />
     </>
   );
 }
---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactStyles.module.css";
+
+function Contact() {
+  return (
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form action=""></form>
+    </section>
+  );
+}
+
+export default Contact;
